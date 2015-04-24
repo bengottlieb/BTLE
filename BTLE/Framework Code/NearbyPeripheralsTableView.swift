f02635c632a8bf24f80080db4d8487646b8a07cb
Ben Gottlieb <ben@standalone.com>
2015-04-24 15:32:57 -0500

    added Nearby Peripherals table view

---
+++ BTLE/Framework Code/NearbyPeripheralsTableView.swift
@@ -0,0 +1,60 @@
+//
+//  NearbyPeripheralsTableView.swift
+//  BTLE
+//
+//  Created by Ben Gottlieb on 4/24/15.
+//  Copyright (c) 2015 Stand Alone, inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SA_Swift
+
+public class NearbyPeripheralsTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
+	deinit { self.removeAsObserver() }
+
+	public required init(coder aDecoder: NSCoder) { super.init(coder: aDecoder); self.setup() }
+	public override init(frame: CGRect) { super.init(frame: frame); self.setup() }
+	
+	public var peripherals: [BTLEPeripheral]? { didSet { self.reload() }}
+	
+	public func reload() { dispatch_async_main() { self.reloadData() }}
+	
+	//=============================================================================================
+	//MARK: Private
+	var actualPeripherals: [BTLEPeripheral] { return self.peripherals ?? self.nearbyPeripherals }
+	var nearbyPeripherals: [BTLEPeripheral] = []
+	
+	func setup() {
+		self.nearbyPeripherals = Array(BTLE.manager.scanner.peripherals)
+		self.delegate = self
+		self.dataSource = self
+		
+		self.addAsObserver(BTLE.notifications.peripheralWasDiscovered, selector: "reloadNearbyPeripherals", object: nil)
+		
+		self.registerNib(UINib(nibName: "NearbyPeripheralsTableViewCell", bundle: NSBundle(forClass: self.dynamicType)), forCellReuseIdentifier: NearbyPeripheralsTableViewCell.identifier)
+	}
+	
+	//=============================================================================================
+	//MARK: Notifications
+	public func reloadNearbyPeripherals() {
+		self.nearbyPeripherals = Array(BTLE.manager.scanner.peripherals)
+		self.reload()
+	}
+
+	
+	//=============================================================================================
+	//MARK: Tableview delegate/datasource
+	public func numberOfSectionsInTableView(tableView: UITableView) -> Int { return 1 }
+	public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return self.actualPeripherals.count }
+	
+	public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		if let cell = tableView.dequeueReusableCellWithIdentifier(NearbyPeripheralsTableViewCell.identifier, forIndexPath: indexPath) as? NearbyPeripheralsTableViewCell {
+			cell.peripheral = self.actualPeripherals[indexPath.row]
+			
+			return cell
+		}
+		return UITableViewCell()
+	}
+}
+
---
+++ BTLE/Framework Code/NearbyPeripheralsTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  NearbyPeripheralsTableViewCell.swift
+//  BTLE
+//
+//  Created by Ben Gottlieb on 4/24/15.
+//  Copyright (c) 2015 Stand Alone, inc. All rights reserved.
+//
+
+import UIKit
+import SA_Swift
+
+
+public class NearbyPeripheralsTableViewCell: UITableViewCell {
+	deinit { self.removeAsObserver() }
+	
+	static let identifier = "NearbyPeripheralsTableViewCell"
+	
+	@IBOutlet var nameLabel: UILabel!
+	@IBOutlet var rssiLabel: UILabel!
+	
+	public var peripheral: BTLEPeripheral? { didSet { self.updateUI() }}
+	
+	
+	public func updateUI() {
+		dispatch_async_main() {
+			if let per = self.peripheral {
+				self.nameLabel.text = per.name
+				self.rssiLabel.text = "\(per.rssi ?? 0)"
+				
+				self.rssiLabel.backgroundColor = per.state == .Connected ? UIColor.blueColor() : UIColor.clearColor()
+				self.rssiLabel.textColor = per.state == .Connected ? UIColor.whiteColor() : UIColor.blackColor()
+			}
+		}
+	}
+	
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+		
+		self.rssiLabel.layer.borderWidth = 2.0
+		self.rssiLabel.layer.borderColor = UIColor.blackColor().CGColor
+		self.rssiLabel.layer.masksToBounds = true
+		
+		self.rssiLabel.layer.cornerRadius = self.rssiLabel.bounds.size.width / 2
+		
+		self.addAsObserver(BTLE.notifications.peripheralDidUpdateRSSI, selector: "updateUI")
+	}
+	
+	
+	
+	
+}
